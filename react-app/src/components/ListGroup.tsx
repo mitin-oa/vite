1c3dac9c6f3dc20d59d336e298e4ac4bab3c2eb5
mitin_oleg <mitin_oa@mail.ru>
2023-08-29 20:34:43 +0200

    add code

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,22 @@
+import { MouseEvent } from "react";
+
+function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokio", "London", "Paris"];
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length == 0 && <p>No items in List</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
