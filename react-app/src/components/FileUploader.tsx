9d64f768568b88a861668c15777ba6ac44928d51
mitin_oleg <mitin_oa@mail.ru>
2023-10-22 19:25:44 +0200

    fix file uploader component

--- react-app/src/components/FileUploader.tsx
+++ react-app/src/components/FileUploader.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import Button from "./Button";
+import InputFile from "./InputFile";
+import MyUploadButton from "./InputFile";
+import InputFiles from "./InputFile";
 
 const FileUploader = () => {
   const [file, setFile] = useState<File | null>(null);
+
+  const [files, setFiles] = useState<any[]>([]);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -17,7 +22,7 @@ const FileUploader = () => {
   return (
     <>
       <div className="col-12">
-        <div className="file-upload">
+        {/* <div className="file-upload">
           <label>
             <input
               type="file"
@@ -28,28 +33,34 @@ const FileUploader = () => {
             />
             <span>Choose file</span>
           </label>
+        </div> */}
+        <InputFiles files={files} setFiles={setFiles} />
+        <div>
+          {files && (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th colSpan={2}>File details</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Name</td>
+                  {/* <td>Type</td> */}
+                  <td>Size</td>
+                </tr>
+
+                {files.map((file) => (
+                  <tr>
+                    <td>{file.originalFile.originalFileName}</td>
+                    {/* <td>{file.type}</td> */}
+                    <td>{(file.originalFile.size / 1024).toFixed(1)} Kbytes</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
-        {file && (
-          <table className="table">
-            <thead>
-              <tr>
-                <th colSpan={3}>File details</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Name</td>
-                <td>Type</td>
-                <td>Size</td>
-              </tr>
-              <tr>
-                <td>{file.name}</td>
-                <td>{file.type}</td>
-                <td>{(file.size / 1024).toFixed(1)} Kbytes</td>
-              </tr>
-            </tbody>
-          </table>
-        )}
       </div>
 
       {file && (
