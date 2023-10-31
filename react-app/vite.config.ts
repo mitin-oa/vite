19364ae20639e609f1ae5b2b64a71f3b268cb8c4
mitin_oleg <mitin_oa@mail.ru>
2023-10-31 17:17:38 +0100

    fix deploy

--- react-app/vite.config.ts
+++ react-app/vite.config.ts
@@ -7,12 +7,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+  /* build: {
     // generate manifest.json in outDir
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
       input: 'src/main.tsx'
     }
-  }
+  } */
 })
