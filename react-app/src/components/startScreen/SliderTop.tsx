52ba4dc527389a581606b262e5eaacc648f23be2
mitin_oleg <mitin_oa@mail.ru>
2024-03-13 19:03:42 +0100

    Make slider on home page

---
+++ react-app/src/components/startScreen/SliderTop.tsx
@@ -0,0 +1,129 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../startScreen/startScreen.scss";
+import Button from "../Button";
+import { Link } from "react-router-dom";
+function SampleNextArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        width: "30px",
+        height: "30px",
+        //background: "black",
+        right: "50px",
+        zIndex: "2",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        //background: "black",
+        left: "50px",
+        zIndex: "2",
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default function SliderTop() {
+  var settings = {
+    dots: true,
+    infinite: true,
+    fade: true,
+    autoplay: true,
+    slickplay: true,
+    speed: 2000,
+    autoplaySpeed: 6000,
+    cssEase: "linear",
+    swipeToSlide: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <div className="slider-container">
+      <Slider {...settings}>
+        <div className="slider-top-card card1">
+          <div className="slider-top-card-shadow">
+            <div
+              className="slider-top-content"
+              style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                zIndex: "10",
+              }}
+            >
+              <h1 className="slider-top-card__text">
+                24/7 on demand contract drafting, review, and negotiation
+                delivered on time and on budget
+              </h1>
+              <Link to="/Services">
+                <Button
+                  children="GET STARTED"
+                  color=""
+                  style={"btn_get_started"}
+                  onClick={() => null}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="slider-top-card card2">
+          <div
+            className="wrapper"
+            style={{ flexDirection: "column", marginLeft: "40%" }}
+          >
+            <h1 className="slider-top-card__text">
+              Seamless automation of your contracts, creation, approval and
+              signature processes and workflows
+            </h1>
+            <Link to="/Services">
+              <Button
+                children="GET STARTED"
+                color=""
+                style={"btn_get_started"}
+                onClick={() => null}
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="slider-top-card card3">
+          <div
+            className="wrapper"
+            style={{ flexDirection: "column", marginLeft: "40%" }}
+          >
+            <h1 className="slider-top-card__text">
+              Smart contract lifecycle management solution SaaS-based or
+              deployed in your SharePoint environment
+            </h1>
+            <Link to="/Services">
+              <Button
+                children="GET STARTED"
+                color=""
+                style={"btn_get_started"}
+                onClick={() => null}
+              />
+            </Link>
+          </div>
+        </div>
+      </Slider>
+    </div>
+  );
+}
--- react-app/src/components/startScreen/startScreen.tsx
+++ react-app/src/components/startScreen/startScreen.tsx
@@ -7,6 +7,7 @@ import { useMediaQuery } from "react-responsive";
 import { useContext, useEffect, useState } from "react";
 import { SignedInContext } from "../../App";
 import { Parallax } from "react-scroll-parallax";
+import SliderTop from "./SliderTop";
 
 export default function StartScreen() {
   const isMobileScreen = useMediaQuery({ query: "(max-width: 1160px" });
@@ -15,168 +16,120 @@ export default function StartScreen() {
 
   return (
     <section className="start_screen">
-      <div className="wrapper start_screen_content d-flex flex-lm-row flex-md-block">
-        <div className="Content">
-          <div
-            className="only_title"
-            style={{
-              display: "flex",
-              width: "100%",
-              fontSize: "55px",
-              fontWeight: "bold",
-              textAlign: "start",
-              lineHeight: "70px",
-              fontFamily: "Montserrat",
-              marginBottom: "0px",
-              color: "#ec720b",
-            }}
-          >
-            <Parallax
-              speed={5}
-              translateY={["-350px", "350px"]}
-              opacity={[7, -2.8]}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  height: "680px",
-                }}
-              >
-                <div
-                  style={{
-                    height: "60px",
-                    minWidth: "380px",
-                    textAlign: "start",
-                    margin: "145px 0",
-                  }}
-                >
-                  24/7 contract
-                  <p
-                    className="only_text"
-                    style={{
-                      textAlign: "start",
-                      margin: "20px 20px 0 0",
-                      color: "white",
-                    }}
-                  >
-                    Your contracts drafted, reviewed, and negotiated by
-                    experienced lawyers when you need it and how you want it.
-                  </p>
-                </div>
-
-                <Link to="/Services">
-                  <Button
-                    children="GET STARTED"
-                    color=""
-                    style={"btn_get_started"}
-                    onClick={() => null}
-                  />
-                </Link>
-              </div>
-            </Parallax>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                fontSize: "55px",
-                fontWeight: "bold",
-                textAlign: "start",
-                lineHeight: "60px",
-                fontFamily: "Montserrat",
-                color: "white",
-              }}
-            >
-              <Parallax
-                opacity={[4, -1.0]}
-                speed={0}
-                translateY={["-100px", "-100px"]}
-              >
-                <a style={{ letterSpacing: "23px" }}>drafting</a>
-              </Parallax>
-              <Parallax
-                opacity={[2.5, -0.5]}
-                speed={0}
-                translateY={["-100px", "-100px"]}
-              >
-                <a style={{ letterSpacing: "6px" }}>negotiation</a>
-              </Parallax>
-              <Parallax
-                opacity={[0.2, 1.0]}
-                speed={0}
-                translateY={["-100px", "-100px"]}
-              >
-                <a style={{ letterSpacing: "6px" }}>automation</a>
-              </Parallax>
-              <Parallax
-                opacity={[-0.2, 1.5]}
-                speed={0}
-                translateY={["-100px", "-100px"]}
-              >
-                <a>management</a>
-              </Parallax>
-              <Parallax
-                opacity={[-0.3, 1.2]}
-                speed={0}
-                translateY={["-100px", "-100px"]}
-              >
-                <a style={{ letterSpacing: "36px" }}>rewiew</a>
-              </Parallax>
-            </div>
-
-            <Parallax
-              speed={5}
-              translateY={["-350px", "350px"]}
-              opacity={[7, -2.8]}
-            >
-              <div
-                style={{
-                  display: "flex",
-                  height: "680px",
-                }}
-              >
-                <div
-                  style={{
-                    height: "60px",
-                    minWidth: "350px",
-                    textAlign: "start",
-                    margin: "145px 0",
-                  }}
-                >
-                  services simplified and delivered on time, and on budget!
-                </div>
-              </div>
-            </Parallax>
-          </div>
-
-          <p
-            className="only_text"
-            style={{
-              margin: "0px 0px 30px",
-              color: "#ec720b",
-            }}
-          >
-            ChronoLegal helps businesses contract and close more deals, on time
-            and on budget! Our attorneys, legal technologists, and consultants
-            live and breathe commercial contracts. It’s all we do and think
-            about. We won’t attempt to fix all your legal and business issues,
-            but we will conquer your contracts!!!
-          </p>
-        </div>
-
-        {/* {!isPhoneScreen ? (
-          <div className="Content">
-            <img
-              className="start_screen_pet"
-              src={StartScreenPic}
-              alt="Picture start screen"
-            />
-          </div>
-        ) : (
-          <></>
-        )} */}
-      </div>
+      <div className="d-flex flex-lm-row flex-md-block"></div>
     </section>
   );
+}
+
+{
+  /* <Parallax
+speed={5}
+translateY={["-350px", "350px"]}
+opacity={[7, -2.8]}
+>
+<div
+  style={{
+    display: "flex",
+    flexDirection: "column",
+    height: "680px",
+  }}
+>
+  <div
+    style={{
+      height: "60px",
+      minWidth: "380px",
+      textAlign: "start",
+      margin: "145px 0",
+    }}
+  >
+    24/7 contract
+    <p
+      className="only_text"
+      style={{
+        textAlign: "start",
+        margin: "20px 20px 0 0",
+        color: "white",
+      }}
+    >
+      Your contracts drafted, reviewed, and negotiated by
+      experienced lawyers when you need it and how you want it.
+    </p>
+  </div>
+
+  
+</div>
+</Parallax>
+<div
+style={{
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  fontSize: "55px",
+  fontWeight: "bold",
+  textAlign: "start",
+  lineHeight: "60px",
+  fontFamily: "Montserrat",
+  color: "white",
+}}
+>
+<Parallax
+  opacity={[4, -1.0]}
+  speed={0}
+  translateY={["-100px", "-100px"]}
+>
+  <a style={{ letterSpacing: "23px" }}>drafting</a>
+</Parallax>
+<Parallax
+  opacity={[2.5, -0.5]}
+  speed={0}
+  translateY={["-100px", "-100px"]}
+>
+  <a style={{ letterSpacing: "6px" }}>negotiation</a>
+</Parallax>
+<Parallax
+  opacity={[0.2, 1.0]}
+  speed={0}
+  translateY={["-100px", "-100px"]}
+>
+  <a style={{ letterSpacing: "6px" }}>automation</a>
+</Parallax>
+<Parallax
+  opacity={[-0.2, 1.5]}
+  speed={0}
+  translateY={["-100px", "-100px"]}
+>
+  <a>management</a>
+</Parallax>
+<Parallax
+  opacity={[-0.3, 1.2]}
+  speed={0}
+  translateY={["-100px", "-100px"]}
+>
+  <a style={{ letterSpacing: "36px" }}>rewiew</a>
+</Parallax>
+</div>
+
+<Parallax
+speed={5}
+translateY={["-350px", "350px"]}
+opacity={[7, -2.8]}
+>
+<div
+  style={{
+    display: "flex",
+    height: "680px",
+  }}
+>
+  <div
+    style={{
+      height: "60px",
+      minWidth: "350px",
+      textAlign: "start",
+      margin: "145px 0",
+    }}
+  >
+    services simplified and delivered on time, and on budget!
+  </div>
+</div>
+</Parallax> */
 }
