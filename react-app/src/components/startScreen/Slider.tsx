d270956e651a4471b4e905f03029a859f077cdd0
mitin_oleg <mitin_oa@mail.ru>
2024-03-15 22:03:59 +0100

    building for production

--- react-app/src/components/startScreen/Slider.tsx
+++ react-app/src/components/startScreen/Slider.tsx
@@ -14,45 +14,47 @@ export default function SimpleSlider() {
   return (
     <div className="slider-container">
       <Slider {...settings}>
-        <div className="slider-card">
-          <p className="slider-card__text">
+        <div className="slider-bottom-card">
+          <p className="slider-bottom-card__text">
             We are now able to win opportunities that we were losing in the past
             because of our workload. Our legal team hasn't expanded, but with
             ChronoLegal, we can scale as needed.
           </p>
-          <p className="slider-card__text">James K., General Counsel</p>
+          <p className="slider-bottom-card__text">James K., General Counsel</p>
         </div>
-        <div className="slider-card">
-          <p className="slider-card__text">
+        <div className="slider-bottom-card">
+          <p className="slider-bottom-card__text">
             This kind of legal support is something every startup needs!
             ChronoLegal was instrumental in helping us close some of our biggest
             deals this year.
           </p>
-          <p className="slider-card__text">Oren S., CEO, Founder</p>
+          <p className="slider-bottom-card__text">Oren S., CEO, Founder</p>
         </div>
-        <div className="slider-card">
-          <p className="slider-card__text">
+        <div className="slider-bottom-card">
+          <p className="slider-bottom-card__text">
             We cannot recommend ChronoLegal highly enough. They are thorough,
             astute, and stay hands-on from start to finish. They care about
             every contract and deal as much as we do.
           </p>
-          <p className="slider-card__text">Mary L., SVP, Procurement</p>
+          <p className="slider-bottom-card__text">Mary L., SVP, Procurement</p>
         </div>
-        <div className="slider-card">
-          <p className="slider-card__text">
+        <div className="slider-bottom-card">
+          <p className="slider-bottom-card__text">
             We started working with ChronoLegal because our business was scaling
             fast but we weren’t ready to increase our legal team’s headcount.
             With ChronoLegal’s help, we have been able to support our sales and
             procurement team adequately and keep our leadership happy.
           </p>
-          <p className="slider-card__text">Diana G., Chief Legal Officer</p>
+          <p className="slider-bottom-card__text">
+            Diana G., Chief Legal Officer
+          </p>
         </div>
-        <div className="slider-card">
-          <p className="slider-card__text">
+        <div className="slider-bottom-card">
+          <p className="slider-bottom-card__text">
             My experience with ChronoLegal was excellent. Their people are
             knowledgeable, kind, and always open for calls and quick updates.
           </p>
-          <p className="slider-card__text">Mike M., VP of Sales</p>
+          <p className="slider-bottom-card__text">Mike M., VP of Sales</p>
         </div>
       </Slider>
     </div>
