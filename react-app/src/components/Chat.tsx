f0ea4764fdebf283f659097b995f6d79b000428a
mitin_oleg <mitin_oa@mail.ru>
2024-05-24 17:06:53 -0700

    add changes to Chat component - remove title

--- react-app/src/components/Chat.tsx
+++ react-app/src/components/Chat.tsx
@@ -1,14 +1,13 @@
 /**
  * VK: Chat Component
- * 
+ *
  * The Chat component is a React functional component designed to provide a instant chat interface
- * using the react-chat-widget library. 
+ * using the react-chat-widget library.
  */
 
-
-import React from 'react';
+import React from "react";
 import { Widget, addResponseMessage } from "react-chat-widget";
-import { sendMessage } from '../fetchScripts/chatRequests'
+import { sendMessage } from "../fetchScripts/chatRequests";
 
 interface ChatProps {
   profileAvatar: string;
@@ -19,32 +18,36 @@ interface ChatProps {
 
 const handleNewUserMessage = async (newMessage: string) => {
   console.log(`New message incoming! ${newMessage}`);
-  
+
   try {
     const response = await sendMessage(newMessage);
-    
+
     if (response.success) {
-      console.log('Server response:', response.message);
+      console.log("Server response:", response.message);
     } else {
-      console.error('Server error:', response.error);
+      console.error("Server error:", response.error);
     }
   } catch (error) {
-    console.error('Failed to send message:', error);
+    console.error("Failed to send message:", error);
   }
 };
 
 addResponseMessage("Welcome to this **awesome** chat!");
 
-const Chat: React.FC<ChatProps> = ({ profileAvatar, title, subtitle, emojis }) => {
+const Chat: React.FC<ChatProps> = ({
+  profileAvatar,
+  title,
+  subtitle,
+  emojis,
+}) => {
   return (
     <div>
-      <h1>{title}</h1>
-      <Widget 
-        handleNewUserMessage={handleNewUserMessage} 
-        profileAvatar={profileAvatar} 
-        title={title} 
-        subtitle={subtitle} 
-        emojis={emojis} 
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        profileAvatar={profileAvatar}
+        title={title}
+        subtitle={subtitle}
+        emojis={emojis}
       />
     </div>
   );
